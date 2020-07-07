cf67479b6975e8eb701aa2f93b7a0611aaf5f472
Clóvis Jr <clovis@goepik.com.br>
2020-07-06 21:05:33 -0300

    server

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../../public')))
 
 app.post('/', (req, res) => {
     // console.log(req.body)
